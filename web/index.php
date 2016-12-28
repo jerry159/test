5c45058777eaf1d1708789893d2f9fcb19f36549
jerry159 <s893042@hotmail.com>
2016-12-28 12:06:16 +0800

    加入時間
    輸入時間 會顯示現在時間

--- web/index.php
+++ web/index.php
@@ -6,8 +6,6 @@ $input = file_get_contents('php://input');
 $json = json_decode($input);
 $event = $json->events[0];
 
-// Show all information, defaults to INFO_ALL
-//phpinfo();
 // 給 LINE 用戶端 相關參數
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('CFvNdr2w47+0oB2QECf7UPB+ttgJCWeXXT+A5sflL+LYmK7nPyncW0pRaAO7DABzNub2MuhamUrtjx39F1nE2sq3pVP0WejYolMKz+dYhb66voJXRuolqJT0wNza3rfT8eLsjxQrB28u0zpD4em2DQdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '0651815f918a41ca3442ed5c8397dbb7']);
@@ -16,9 +14,16 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '0651815f918a41ca3442e
 if ("message" == $event->type) {            //一般的なメッセージ(文字・イメージ・音声・位置情報・スタンプ含む)
     //テキストメッセージにはオウムで返す
     if ("text" == $event->message->type) {
-       $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->message->text);
+	   	$order = $event->message->text ;
+	   if("時間" == $event->message->text ){
+		   $newtime=time();
+		   $time = "現在時間:".date("Y-m-d H:i:s",$newtime);
+		   $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($time);
+	   }else{
+		$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("請確認你輸入指令");
 	   //$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder("https://jpeg.org/images/jpeg-home.jpg","https://jpeg.org/images/jpeg-home.jpg");//圖片
-    }elseif("sticker" == $event->message->type){
+	   }
+	}elseif("sticker" == $event->message->type){
 		$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder("1","1");
 	}else {
         $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("我看不懂你 @@");
@@ -38,14 +43,6 @@ syslog(LOG_EMERG, print_r($event->replyToken, true));
 syslog(LOG_EMERG, print_r($response, true));
 return;
 
-//$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-//$response = $bot->replyMessage('<reply token>', $textMessageBuilder);
-//if ($response->isSucceeded()) {
-//    echo 'Succeeded!';
-//    return;
-//}
 
-// Failed
-//echo $response->getHTTPStatus . ' ' . $response->getRawBody();
 
 ?>
