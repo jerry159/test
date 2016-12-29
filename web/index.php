dd3029513de2985ff54242937e2e0f49f200837c
jerry159 <s893042@hotmail.com>
2016-12-29 10:51:20 +0800

    change Token
    Token

--- web/index.php
+++ web/index.php
@@ -1,5 +1,4 @@
 <?php
-/*
 
 date_default_timezone_set('Asia/Taipei');
 //載入LINE BOT SDK
@@ -10,7 +9,7 @@ $json = json_decode($input);
 $event = $json->events[0];
 
 //設定LINE bot 相關參數
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('CFvNdr2w47+0oB2QECf7UPB+ttgJCWeXXT+A5sflL+LYmK7nPyncW0pRaAO7DABzNub2MuhamUrtjx39F1nE2sq3pVP0WejYolMKz+dYhb66voJXRuolqJT0wNza3rfT8eLsjxQrB28u0zpD4em2DQdB04t89/1O/w1cDnyilFU=');
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('sI+voOXTEQss74igmy+TAiWwKzgssW4xHn20K/SfFTt42k5tkrvPi04N13n6B8MXNub2MuhamUrtjx39F1nE2sq3pVP0WejYolMKz+dYhb6X4CeKbxv7rAb05/72fCeRP38QBI/gJpYoV2TvboDPoQdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '0651815f918a41ca3442ed5c8397dbb7']);
 
 $servertext = "看不懂你說的，目前提供服務輸入\n '時間'-->可以現在時間\n '目前活動'\n";
@@ -55,38 +54,5 @@ $response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
 syslog(LOG_EMERG, print_r($event->replyToken, true));
 syslog(LOG_EMERG, print_r($response, true));
 return;
-*/
-
-require_once __DIR__ . '/vendor/autoload.php';
-//POST
-$input = file_get_contents('php://input');
-$json = json_decode($input);
-$event = $json->events[0];
-
-// 給 LINE 用戶端 相關參數
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('sI+voOXTEQss74igmy+TAiWwKzgssW4xHn20K/SfFTt42k5tkrvPi04N13n6B8MXNub2MuhamUrtjx39F1nE2sq3pVP0WejYolMKz+dYhb6X4CeKbxv7rAb05/72fCeRP38QBI/gJpYoV2TvboDPoQdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '0651815f918a41ca3442ed5c8397dbb7']);
-//イベントタイプ判別
-if ("message" == $event->type) {            //一般的なメッセージ(文字・イメージ・音声・位置情報・スタンプ含む)
-    //テキストメッセージにはオウムで返す
-    if ("text" == $event->message->type) {
-        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->message->text);
-    } else {
-        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("ごめん、わかんなーい(*´ω｀*)");
-    }
-} elseif ("follow" == $event->type) {        //お友達追加時
-    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("よろしくー");
-} elseif ("join" == $event->type) {           //グループに入ったときのイベント
-    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('こんにちは よろしくー');
-} elseif ('beacon' == $event->type) {         //Beaconイベント
-    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('Godanがいんしたお(・∀・) ');
-} else {
-    //なにもしない
-}
-$response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
-syslog(LOG_EMERG, print_r($event->replyToken, true));
-syslog(LOG_EMERG, print_r($response, true));
-return;
-
 
 ?>
