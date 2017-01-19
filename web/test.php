2a0c03f817b0363adafd25ef7d56078224133eb6
jerry159 <s893042@hotmail.com>
2017-01-19 17:22:22 +0800

    111
    111

--- web/test.php
+++ web/test.php
@@ -1,11 +1,5 @@
 <?php
 
-ini_set('session.save_handler', 'memcached');
-ini_set('session.save_path', 'PERSISTENT=myapp_session ' . getenv('MEMCACHIER_SERVERS'));
-ini_set('memcached.sess_binary', 1);
-ini_set('memcached.sess_sasl_username', getenv('MEMCACHIER_USERNAME'));
-ini_set('memcached.sess_sasl_password', getenv('MEMCACHIER_PASSWORD'));
-
 session_start();
 if (!isset($_SESSION['count'])) {
     $_SESSION['count'] = 0;
